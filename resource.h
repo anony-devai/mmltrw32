206b91067e1982244bb70f4afbc8c92b247b28c9
anony-devai <koko-r@hotmail.co.jp>
2026-07-01 11:41:18 +0900

    Refactor resource.h by cleaning up comments
    Removed commented-out sections and organized control IDs.

--- resource.h
+++ resource.h
@@ -1,47 +1,36 @@
 // resource.h
 
 // ------------------------------
-// ダイアログ
+// Dialog
 // ------------------------------
 #define IDD_MAIN_DIALOG        100
 
 // ------------------------------
-// コントロール ID（昇順で整理済み）
+// Controls
 // ------------------------------
 #define IDC_STATIC_FILENAME    1000
-
-// Shift
 #define IDC_SLIDER_SHIFT       1001
 #define IDC_EDIT_SHIFT         1002
 #define IDC_SPIN_SHIFT         1003
-
-// Format
 #define IDC_CHECK_FORMAT       1004
-
-// Octave
 #define IDC_CHECK_REL          1005
 #define IDC_CHECK_ABS          1006
-
-// Auto-save
 #define IDC_CHECK_AUTOSAVE     1007
-
-// Save 系
 #define IDC_BUTTON_SAVE_AUTO   1008
 #define IDC_BUTTON_SAVE_AS     1009
-
-// Labels
 #define IDC_STATIC_KEY         1010
 #define IDC_STATIC_OPTION      1011
-// 4行目に [D-ch] 追加
-#define IDC_CHECK_DSHIFT       1012    // [D-ch] ← 新規追加
+#define IDC_CHECK_DSHIFT       1012
 
-/* このVerはメニュー表示なしコメント化
 // ------------------------------
-// メニュー
+// Hidden Menu
 // ------------------------------
 #define IDR_MAINMENU           200
+
+// File
 #define IDM_FILE_OPEN          201
 #define IDM_FILE_EXIT          202
-#define IDM_FILE_SAVE          203
-#define IDM_FILE_SAVE_AS       204
-*/
+
+// Language
+#define IDM_LANG_EN            210
+#define IDM_LANG_JP            211
